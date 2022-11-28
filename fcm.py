4f9e7691bf248af1fa97c77cb10546b790110f4b
jjaegii <hn06038@gmail.com>
2022-11-28 09:47:52 +0900

    modify device token

--- fcm.py
+++ fcm.py
@@ -6,8 +6,12 @@ cred = credentials.Certificate("./config/fcm_key.json")
 firebase_admin.initialize_app(cred)
 
 # 안드로이드에서 나온 토큰 값
-registration_token = 'cl1sNiNVIG8:APA91bEQ6USv7nu1Wwi4h--ewAK6E-l5TWhE85g3oBdUUfhVBp2TXhk4hVJdofpZwxGviEuFa4mZ4dUpKZtItZVxCXX6wo9NBgStb0sO2ZtPVxgCIgJmpEmOjoaeV8CKkZgOJurCqlEq'
 
+# 에뮬레이터
+# registration_token = 'fvK5K78WpiI:APA91bFxJyEcNr-AC6pYR0wBvUwaDk5FNudrw065wSRrs0WqACCkw4wT6qeI3E77FOJyf7w3r51sMOzaim8L8SpeMQm1gSpcJoBXMKfmVbl5C4XtYTcyD7b5FPIhO6AHmms1e-FbzSoQ'
+
+# 실물
+registration_token = 'cSH2MKuxkHw:APA91bGYcfIaQQtmikKjTzSq4z_W3N_f-8hY0D0CZbTko_w66vVEtcQNWCJ8QZubHSL7PYM69QY6otqmuuIQHZyxvdesZV9h0bPtfuZvEofONh282X24Dpgp7UsLEFRMFKMPrhUTz7uq'
 
 def fcm_send(action):
     message = messaging.Message(
